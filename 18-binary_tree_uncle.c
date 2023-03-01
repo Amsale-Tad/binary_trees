ca5b48cca6852e02f6503d11df7ad1ad7459e247
Amsale-Tad <amsalebeykaso@gmail.com>
2023-03-01 10:27:14 +0300

    binary new

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,9 +6,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (node == NULL ||
+	    node->parent == NULL ||
+	    node->parent->parent == NULL)
 		return (NULL);
-	if (node->parent == node->parent->parent->right)
-		return (node->parent->parent->left);
-	return (node->parent->parent->right);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
